9e20b81283ac790bc75596047e3886b9e89603d9
Emerson Patryck <emeps@outlook.com>
2024-03-28 01:22:06 -0300

    feat: add manufacturer of vehicle model

---
+++ src/main/java/com/github/emeps/cars/models/Manufacturer.java
@@ -0,0 +1,20 @@
+package com.github.emeps.cars.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "manufacturer")
+public class Manufacturer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 155)
+    private String name;
+}
