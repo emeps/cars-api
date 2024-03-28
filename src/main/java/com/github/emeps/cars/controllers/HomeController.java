a2b75fdfab2c72c70e2a18f8104849b6f7b1554d
Emerson Patryck <emeps@outlook.com>
2024-03-28 01:20:59 -0300

    feat: add home controller for test

---
+++ src/main/java/com/github/emeps/cars/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.github.emeps.cars.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public Map<String, ?> index(){
+        return  Map.of("message", "hello");
+    }
+
+}
