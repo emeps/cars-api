aab52ba74fba37ebf6b0849ef6a6a78dbf50cc23
Emerson Patryck <emeps@outlook.com>
2024-03-30 02:08:25 -0300

    chore: change mock by crud function

--- src/main/java/com/github/emeps/cars/controllers/VehicleController.java
+++ src/main/java/com/github/emeps/cars/controllers/VehicleController.java
@@ -1,72 +1,41 @@
 package com.github.emeps.cars.controllers;
 
-import com.github.emeps.cars.models.Manufacturer;
-import com.github.emeps.cars.models.Model;
+import com.github.emeps.cars.DTOS.CreateVehicleData;
 import com.github.emeps.cars.models.Vehicle;
+import com.github.emeps.cars.services.VehicleService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/vehicles")
+@AllArgsConstructor
 public class VehicleController {
+
+    private final VehicleService vehicleService;
     @GetMapping
     public  List<Vehicle> index(){
-//        return  List.of(
-//                new Vehicle(
-//                        1L,
-//                        10000,
-//                        "Azul",
-//                        "Novo",
-//                        true,
-//                        1987,
-//                        new Model(
-//                                1L,
-//                                "Fusca",
-//                                new Manufacturer(
-//                                        1L,
-//                                        "Volks"
-//                                )
-//                        )
-//                )
-//        );
-        return  null;
+        return  vehicleService.list();
     }
 
     @GetMapping("/{id}")
-    public List<Vehicle> show(@PathVariable("id") Long id){
-//        return  List.of(
-//                new Vehicle(
-//                        1L,
-//                        10000,
-//                        "Azul",
-//                        "Novo",
-//                        true,
-//                        1987,
-//                        new Model(
-//                                1L,
-//                                "Fusca",
-//                                new Manufacturer(
-//                                        1L,
-//                                        "Volks"
-//                                )
-//                        )
-//                )
-//        );
-        return null;
+    public Vehicle show(@PathVariable("id") Long id){
+        return vehicleService.show(id);
     }
 
-    @PostMapping
-    public Vehicle create(Vehicle vehicle){
-        return null;
+    @PostMapping("/create")
+    public Vehicle create(@RequestBody CreateVehicleData data ){
+        return vehicleService.create(data);
     }
 
     @PutMapping("/{id}")
     public Vehicle update(Vehicle vehicle, @PathVariable("id")Long id){
-        return null;
+        return vehicleService.update(vehicle, id);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(Vehicle vehicle, @PathVariable("id")Long id){
+    public void delete(@PathVariable("id")Long id){
+        vehicleService.deleteById(id);
     }
 }
