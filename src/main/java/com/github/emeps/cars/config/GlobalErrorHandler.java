ce3d6852d4c12060b7d099f316b6fe618822f2b1
Emerson Patryck <emeps@outlook.com>
2024-03-31 17:14:52 -0300

    feat: implements config to validation error and default error to response

--- src/main/java/com/github/emeps/cars/config/GlobalErrorHandler.java
+++ src/main/java/com/github/emeps/cars/config/GlobalErrorHandler.java
@@ -1,22 +1,28 @@
 package com.github.emeps.cars.config;
 
+import com.github.emeps.cars.errors.DefaultErrorCars;
+import com.github.emeps.cars.errors.ValidationError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Map;
-import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 
 @RestControllerAdvice
 public class GlobalErrorHandler {
+
     @ExceptionHandler(ResponseStatusException.class)
-    public  ResponseEntity<Map<String, ?>> status (ResponseStatusException exception){
-        return ResponseEntity.status(exception.getStatusCode()).body(
-                Map.of(
-                        "status", exception.getStatusCode().value(),
-                        "message", Optional.ofNullable(exception.getReason()).orElse("")
-                )
-        );
+    public  ResponseEntity<DefaultErrorCars> handleResponseStatusException (ResponseStatusException exception){
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(new DefaultErrorCars(exception.getReason(), exception.getStatusCode()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultErrorCars> handleValidation(MethodArgumentNotValidException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new DefaultErrorCars(HttpStatus.BAD_REQUEST, ValidationError.of(exception)));
+    }
+
 }
